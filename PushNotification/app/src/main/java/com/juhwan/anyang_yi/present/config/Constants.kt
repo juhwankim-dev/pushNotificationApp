5ff27805a97bbb16626120714e8a92a87535fe57
Juhwan Kim <juhwan.dev@gmail.com>
2022-05-29 21:54:03 +0900

    refactor: :zap: url 주소, 날짜 계산, Result, 상태 util 추가 (#10)

---
+++ PushNotification/app/src/main/java/com/juhwan/anyang_yi/present/config/Constants.kt
@@ -0,0 +1,12 @@
+package com.juhwan.anyang_yi.present.config
+
+object Constants {
+    const val ARI_BASE_URL = "https://ari.anyang.ac.kr/user/bbs/notice/"
+    const val CONTACT_BASE_URL = "http://www.anyang.ac.kr/contents/"
+    const val KAKAO_BASE_URL = "https://pf-wapi.kakao.com/web/profiles/"
+    const val NONSUBJECT_BASE_URL = "https://ari.anyang.ac.kr/user/subject/nsubject/"
+    const val NONSUBJECT_IMAGE_BASE_URL = "http://ari.anyang.ac.kr"
+    const val SCHEDULE_BASE_URL = "https://webcache.googleusercontent.com/"
+    const val UNIV_BASE_URL = "https://www.anyang.ac.kr/main/communication/"
+
+}
---
+++ PushNotification/app/src/main/java/com/juhwan/anyang_yi/present/utils/Status.kt
@@ -0,0 +1,8 @@
+package com.juhwan.anyang_yi.present.utils
+
+enum class Status {
+    SUCCESS,
+    ERROR,
+    FAIL,
+    LOADING
+}
