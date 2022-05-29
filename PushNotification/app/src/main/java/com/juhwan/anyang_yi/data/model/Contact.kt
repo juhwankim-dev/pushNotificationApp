d30036a37e1046a4163de975d87e47a7a5dc71a3
Juhwan Kim <juhwan.dev@gmail.com>
2022-05-29 21:45:33 +0900

    refactor: :zap: 필요하지 않은 파일 삭제(#10)

--- PushNotification/app/src/main/java/com/juhwan/anyang_yi/data/model/Contact.kt
+++
@@ -1,3 +0,0 @@
-package com.juhwan.anyang_yi.data.model
-
-data class Contact(var lClass: String, var mClass: String, var sClass: String, var tel: String)
--- PushNotification/app/src/main/java/com/juhwan/anyang_yi/data/model/initialKeyword.kt
+++
@@ -1,3 +0,0 @@
-package com.juhwan.anyang_yi.data.model
-
-data class InitialKeyword(var keyword: String, var isSelected: Boolean)
