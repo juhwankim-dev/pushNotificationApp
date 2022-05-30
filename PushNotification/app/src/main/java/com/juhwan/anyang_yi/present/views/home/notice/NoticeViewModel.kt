8c5a33d0cb96ab9ef91218029f7c4482d031aa17
Juhwan Kim <juhwan.dev@gmail.com>
2022-05-30 22:26:27 +0900

    refactor: :zap: viewModel 리팩터링(#12)

--- PushNotification/app/src/main/java/com/juhwan/anyang_yi/present/views/home/notice/NoticeViewModel.kt
+++ PushNotification/app/src/main/java/com/juhwan/anyang_yi/present/views/home/notice/NoticeViewModel.kt
@@ -1,25 +1,69 @@
 package com.juhwan.anyang_yi.present.views.home.notice
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.juhwan.anyang_yi.domain.model.Ari
+import com.juhwan.anyang_yi.domain.model.Nonsubject
+import com.juhwan.anyang_yi.domain.model.Univ
+import com.juhwan.anyang_yi.domain.usecase.ari.GetAriListUseCase
+import com.juhwan.anyang_yi.domain.usecase.nonsubject.GetNonsubjectListUseCase
+import com.juhwan.anyang_yi.domain.usecase.univ.GetUnivListUseCase
+import com.juhwan.anyang_yi.present.utils.Result
+import com.juhwan.anyang_yi.present.utils.Status
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class NoticeViewModel : ViewModel()  {
-    private var mainNoticeRepository = MainNoticeRepository()
-    private var ariNoticeRepository = AriNoticeRepository()
+@HiltViewModel
+class NoticeViewModel @Inject constructor(
+    private val getAriListUseCase: GetAriListUseCase,
+    private val getNonsubjectListUseCase: GetNonsubjectListUseCase,
+    private val getUnivListUseCase: GetUnivListUseCase
+): ViewModel() {
+    private val _AriNoticeList = MutableLiveData<List<Ari>>()
+    val ariNoticeList: LiveData<List<Ari>> get() = _AriNoticeList
 
-    private val initMainNotice: LiveData<Result>
-        get() = mainNoticeRepository._initMainNotice
+    private val _NonsubjectNoticeList = MutableLiveData<List<Nonsubject>>()
+    val nonsubjectNoticeList: LiveData<List<Nonsubject>> get() = _NonsubjectNoticeList
 
-    init {
-        loadInitialData()
+    private val _UnivNoticeList = MutableLiveData<List<Univ>>()
+    val univNoticeList: LiveData<List<Univ>> get() = _UnivNoticeList
+
+    private val _problem = MutableLiveData<Result<Any>>()
+    val problem: LiveData<Result<Any>> get() = _problem
+
+    fun getAriNoticeList(page: Int) {
+        viewModelScope.launch {
+            val result = getAriListUseCase(page)
+            if(result.status == Status.SUCCESS) {
+                result.data.let { _AriNoticeList.postValue(it) }
+            } else {
+                _problem.postValue(result)
+            }
+        }
     }
 
-    fun loadInitialData() {
-        mainNoticeRepository.loadInitialMainNotice()
-        ariNoticeRepository.loadInitialAriNotice()
+    fun getNonsubjectNoticeList() {
+        viewModelScope.launch {
+            val result = getNonsubjectListUseCase()
+            if(result.status == Status.SUCCESS) {
+                result.data.let { _NonsubjectNoticeList.postValue(it) }
+            } else {
+                _problem.postValue(result)
+            }
+        }
     }
 
-    fun getAll(): LiveData<Result> {
-        return initMainNotice
+    fun getUnivNoticeList() {
+        viewModelScope.launch {
+            val result = getUnivListUseCase()
+            if(result.status == Status.SUCCESS) {
+                result.data.let { _UnivNoticeList.postValue(it) }
+            } else {
+                _problem.postValue(result)
+            }
+        }
     }
 }
--- PushNotification/app/src/main/java/com/juhwan/anyang_yi/present/views/home/notice/nonsubject/NonsubjectViewModel.kt
+++ PushNotification/app/src/main/java/com/juhwan/anyang_yi/present/views/home/notice/nonsubject/NonsubjectViewModel.kt
@@ -1,13 +1,11 @@
-package com.juhwan.anyang_yi.present.views.home.notice.ari
+package com.juhwan.anyang_yi.present.views.home.notice.nonsubject
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.juhwan.anyang_yi.domain.model.Ari
-import com.juhwan.anyang_yi.domain.usecase.ari.GetAriListUseCase
+import com.juhwan.anyang_yi.domain.model.Nonsubject
 import com.juhwan.anyang_yi.domain.usecase.nonsubject.GetNonsubjectListUseCase
-import com.juhwan.anyang_yi.domain.usecase.univ.GetUnivListUseCase
 import com.juhwan.anyang_yi.present.utils.Result
 import com.juhwan.anyang_yi.present.utils.Status
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -15,22 +13,21 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class AriViewModel @Inject constructor(
-    private val getAriListUseCase: GetAriListUseCase,
+class NonsubjectViewModel @Inject constructor(
     private val getNonsubjectListUseCase: GetNonsubjectListUseCase,
-    private val getUnivListUseCase: GetUnivListUseCase
 ): ViewModel() {
-    private val _AriNoticeList = MutableLiveData<List<Ari>>()
-    val ariNoticeList: LiveData<List<Ari>> get() = _AriNoticeList
+
+    private val _NonsubjectNoticeList = MutableLiveData<List<Nonsubject>>()
+    val nonsubjectNoticeList: LiveData<List<Nonsubject>> get() = _NonsubjectNoticeList
 
     private val _problem = MutableLiveData<Result<Any>>()
     val problem: LiveData<Result<Any>> get() = _problem
 
-    fun getAriNoticeList(page: Int) {
+    fun getNonsubjectNoticeList() {
         viewModelScope.launch {
-            val result = getAriListUseCase(page)
+            val result = getNonsubjectListUseCase()
             if(result.status == Status.SUCCESS) {
-                result.data.let { _AriNoticeList.postValue(it) }
+                result.data.let { _NonsubjectNoticeList.postValue(it) }
             } else {
                 _problem.postValue(result)
             }
