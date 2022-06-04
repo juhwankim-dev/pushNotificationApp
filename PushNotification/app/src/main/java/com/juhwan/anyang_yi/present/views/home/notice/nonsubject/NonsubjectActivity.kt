c374aea07acd3c2931e2d69e3058711898d87470
juhwankim-dev <juhwan.dev@gmail.com>
2022-06-04 23:15:11 +0900

    refactor: :zap: 공지사항을 관리하는 activity, fragment 리팩터링(#14)

--- PushNotification/app/src/main/java/com/juhwan/anyang_yi/present/views/home/notice/nonsubject/NonsubjectActivity.kt
+++ PushNotification/app/src/main/java/com/juhwan/anyang_yi/present/views/home/notice/nonsubject/NonsubjectActivity.kt
@@ -1,18 +1,34 @@
 package com.juhwan.anyang_yi.present.views.home.notice.nonsubject
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import com.juhwan.anyang_yi.R
 import com.juhwan.anyang_yi.databinding.ActivityNonsubjectBinding
 import com.juhwan.anyang_yi.present.config.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class NonsubjectActivity : BaseActivity<ActivityNonsubjectBinding>(R.layout.activity_nonsubject) {
+    private val viewModel: NonsubjectViewModel by viewModels()
     private lateinit var nonsubjectAdapter: NonsubjectAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initRecyclerView()
 
+        initView()
+        initEvent()
+        viewModel.getNonsubjectNoticeList()
+    }
+
+    private fun initView(){
+        binding.rvNonsubject.layoutManager = GridLayoutManager(this, 2)
+        nonsubjectAdapter = NonsubjectAdapter()
+        binding.rvNonsubject.adapter = nonsubjectAdapter
+
+    }
+
+    private fun initEvent() {
         binding.radioGroupFilter.setOnCheckedChangeListener { _, checkedId ->
             when(checkedId){
                 R.id.rb_new -> nonsubjectAdapter.arrangeList(0)
@@ -20,16 +36,17 @@ class NonsubjectActivity : BaseActivity<ActivityNonsubjectBinding>(R.layout.acti
             }
         }
 
-        binding.tvNumber.text = InitialRepository.apply.size.toString()
+        viewModel.nonsubjectNoticeList.observe(this) {
+            nonsubjectAdapter.setList(it)
+            binding.tvNumber.text = it.size.toString()
+        }
+
+        viewModel.problem.observe(this) {
+            showToastMessage(resources.getString(R.string.network_error))
+        }
+
         binding.ivBack.setOnClickListener {
             finish()
         }
-    }
-
-    private fun initRecyclerView(){
-        binding.rvNonsubject.layoutManager = GridLayoutManager(this, 2)
-        nonsubjectAdapter = NonsubjectAdapter()
-        binding.rvNonsubject.adapter = nonsubjectAdapter
-        nonsubjectAdapter.setList(InitialRepository.apply)
     }
 }
--- PushNotification/app/src/main/java/com/juhwan/anyang_yi/present/views/home/notice/univ/UnivActivity.kt
+++ PushNotification/app/src/main/java/com/juhwan/anyang_yi/present/views/home/notice/univ/UnivActivity.kt
@@ -2,37 +2,46 @@ package com.juhwan.anyang_yi.present.views.home.notice.univ
 
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.tabs.TabLayout
 import com.juhwan.anyang_yi.R
 import com.juhwan.anyang_yi.databinding.ActivityUnivBinding
 import com.juhwan.anyang_yi.present.config.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class UnivActivity : BaseActivity<ActivityUnivBinding>(R.layout.activity_univ) {
-    private val model: UnivViewModel by viewModels()
+    private val viewModel: UnivViewModel by viewModels()
     private lateinit var univAdapter: UnivAdapter
-    private var page = 1
-    private var bcIdx = "0"
     private var isListEmpty = true
+    private var categoryId = ALL
+    private var offset = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         initRecyclerView()
         initTabLayout()
-        model.loadMainNotice(page, bcIdx)
+        viewModel.getUnivNoticeList(categoryId, offset)
 
+        initEvent()
+    }
+
+    private fun initEvent() {
         binding.ivBack.setOnClickListener {
             finish()
         }
 
-        model.getAll().observe(this, Observer {
-            univAdapter.setList(it.resultList)
+        viewModel.univNoticeList.observe(this) {
+            univAdapter.setList(it)
             isListEmpty = false
-            univAdapter.notifyItemRangeInserted((page - 1) * 15, 15)
-        })
+            univAdapter.notifyItemRangeInserted(offset * 10, 10)
+        }
+
+        viewModel.problem.observe(this) {
+            showToastMessage(resources.getString(R.string.network_error))
+        }
 
         binding.rvUnivNotice.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
@@ -46,7 +55,7 @@ class UnivActivity : BaseActivity<ActivityUnivBinding>(R.layout.activity_univ) {
                     lastVisibleItemPosition == itemTotalCount && !isListEmpty
                 ) {
                     univAdapter.deleteLoading()
-                    model.loadMainNotice(++page, bcIdx)
+                    viewModel.getUnivNoticeList(categoryId, ++offset)
                 }
             }
         })
@@ -70,23 +79,31 @@ class UnivActivity : BaseActivity<ActivityUnivBinding>(R.layout.activity_univ) {
             }
 
             override fun onTabSelected(tab: TabLayout.Tab?) {
-                // bcIdx
-                // 전체(0), 대학교(20), 학사(80), 학사(21), 취업정보(23), 입찰채용(24)
-                when(tab!!.position){
-                    0 -> bcIdx = "0"
-                    1 -> bcIdx = "20"
-                    2 -> bcIdx = "80"
-                    3 -> bcIdx = "21"
-                    4 -> bcIdx = "23"
-                    5 -> bcIdx = "24"
+                categoryId = when(tab!!.position){
+                    1 -> UNIV
+                    2 -> BACHELOR
+                    3 -> GRADUATE_SCHOOL
+                    4 -> JOB
+                    5 -> BID
+                    else -> ALL
                 }
 
-                page = 1
-                univAdapter.resetList(bcIdx)
+                offset = 1
+                univAdapter.resetList()
                 isListEmpty = true
                 univAdapter.notifyDataSetChanged()
-                model.loadMainNotice(page, bcIdx)
+                viewModel.getUnivNoticeList(categoryId, offset)
             }
         })
+    }
+
+    // 전체(null), 대학교(17), 학사(18), 대학원(19), 취업정보(20), 입찰채용(21)
+    companion object {
+        const val ALL = "-1"
+        const val UNIV = "17"
+        const val BACHELOR = "18"
+        const val GRADUATE_SCHOOL = "19"
+        const val JOB = "20"
+        const val BID = "21"
     }
 }
