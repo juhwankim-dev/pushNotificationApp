92b51f7c86feeb14b7b9e20112bd94e5c474e694
juhwankim-dev <juhwan.dev@gmail.com>
2022-06-09 01:18:15 +0900

    refactor: :zap: 키워드 관리 관련 파일 리팩터링(#21)

--- PushNotification/app/src/main/java/com/juhwan/anyang_yi/present/config/ApplicationClass.kt
+++ PushNotification/app/src/main/java/com/juhwan/anyang_yi/present/config/ApplicationClass.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.messaging.FirebaseMessaging
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -11,6 +12,7 @@ class ApplicationClass : Application() {
     companion object {
         lateinit var databaseReference: DatabaseReference
         lateinit var authReference: FirebaseAuth
+        lateinit var fcmReference: FirebaseMessaging
     }
 
     override fun onCreate() {
@@ -18,5 +20,6 @@ class ApplicationClass : Application() {
 
         databaseReference = FirebaseDatabase.getInstance().reference
         authReference = FirebaseAuth.getInstance()
+        fcmReference = FirebaseMessaging.getInstance()
     }
 }
--- PushNotification/app/src/main/java/com/juhwan/anyang_yi/present/views/home/keyword/KeywordActivity.kt
+++ PushNotification/app/src/main/java/com/juhwan/anyang_yi/present/views/home/keyword/KeywordActivity.kt
@@ -3,40 +3,47 @@ package com.juhwan.anyang_yi.present.views.home.keyword
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
+import android.view.WindowManager
 import androidx.activity.viewModels
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.Observer
-import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.github.kimcore.inko.Inko
-import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
-import com.google.firebase.messaging.FirebaseMessaging
 import com.juhwan.anyang_yi.R
-import com.juhwan.anyang_yi.data.db.Keyword
+import com.juhwan.anyang_yi.data.model.KeywordEntity
 import com.juhwan.anyang_yi.databinding.ActivityKeywordBinding
 import com.juhwan.anyang_yi.present.config.ApplicationClass.Companion.databaseReference
+import com.juhwan.anyang_yi.present.config.ApplicationClass.Companion.fcmReference
 import com.juhwan.anyang_yi.present.config.BaseActivity
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import java.util.regex.Pattern
+import com.juhwan.anyang_yi.present.utils.KeywordChecker
+import dagger.hilt.android.AndroidEntryPoint
 
-const val KEYWORD_LIMIT = 10
-
+@AndroidEntryPoint
 class KeywordActivity : BaseActivity<ActivityKeywordBinding>(R.layout.activity_keyword), DeleteButtonListener, SignUpListener {
-
     private lateinit var map: Map<String, String>// 서버에 있는 키워드를 가져와서 저장할 변수
-    private val model: KeywordViewModel by viewModels()
+    private val viewModel: KeywordViewModel by viewModels()
     private lateinit var adapter: KeywordAdapter
-    private var myKeywordList = arrayListOf<Keyword>()
+    private var registeredKeywordList = arrayListOf<KeywordEntity>()
     private val inko = Inko()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        initView()
+        initEvent()
+    }
+
+    private fun initView() {
+        binding.rvKeyword.layoutManager = LinearLayoutManager(this)
+        adapter = KeywordAdapter(this)
+        binding.rvKeyword.adapter = adapter
+    }
+
+    private fun initEvent() {
         databaseReference
             .child("keywords")
             .addValueEventListener(object : ValueEventListener {
@@ -49,45 +56,9 @@ class KeywordActivity : BaseActivity<ActivityKeywordBinding>(R.layout.activity_k
                 }
             })
 
-        initRecyclerView()
-
-//        model.getAll().observe(this, Observer {
-//            adapter.setList(it)
-//            adapter.notifyDataSetChanged()
-//            binding.tvRegisteredKeyword.text = it.size.toString()
-//            myKeywordList.clear()
-//            myKeywordList.addAll(it)
-//        })
-//
-//        model.getResult().observe(this, Observer{
-//            var enteredKeyword = binding.etKeyword.text.toString()
-//
-//            if(it.resultList.isEmpty()){
-//                hideProgress()
-//                val dialog = KeywordDialog(this, this)
-//                dialog.myDig(enteredKeyword)
-//            } else {
-//                if (isValidKeyword(enteredKeyword)) {
-//                    subscribe(enteredKeyword)
-//                }
-//            }
-//        })
-//
-//        binding.btnSubscribe.setOnClickListener {
-//            showProgress()
-//            var enteredKeyword = binding.etKeyword.text.toString()
-//            model.searchKeyword(enteredKeyword) // 공지 이력 확인
-//        }
-
         binding.etKeyword.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(p0: Editable?) {
-
-            }
-
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-            }
-
+            override fun afterTextChanged(p0: Editable?) {}
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 if (p0.toString().trim().isNotEmpty()) {
                     binding.btnSubscribe.isEnabled = true
@@ -99,58 +70,59 @@ class KeywordActivity : BaseActivity<ActivityKeywordBinding>(R.layout.activity_k
             }
         })
 
+        viewModel.readKeywordList().observe(this) {
+            adapter.setList(it)
+            binding.tvRegisteredKeyword.text = it.size.toString()
+            registeredKeywordList.clear()
+            registeredKeywordList.addAll(it)
+        }
+
+        binding.btnSubscribe.setOnClickListener {
+            var keyword = binding.etKeyword.text.toString()
+            try {
+                KeywordChecker.check(keyword, registeredKeywordList)
+                viewModel.getSearchResultList(keyword, 0)
+            } catch (e: Exception) {
+                showToastMessage(e.message.toString())
+            }
+        }
+
+        viewModel.searchResultList.observe(this) {
+            var keyword = binding.etKeyword.text.toString()
+
+            if(it.isEmpty()){
+                KeywordDialog(this, this).createDialog(keyword)
+            } else {
+                subscribe(keyword)
+            }
+        }
+
+        viewModel.problem.observe(this) {
+            showToastMessage(resources.getString(R.string.database_error))
+            Log.d("주환", "initEvent: ${it.message}")
+        }
+
         binding.ivBack.setOnClickListener {
             finish()
         }
     }
 
-    private fun initRecyclerView() {
-        binding.rvKeyword.layoutManager = LinearLayoutManager(this)
-        adapter = KeywordAdapter(this)
-        binding.rvKeyword.adapter = adapter
-    }
+    private fun subscribe(keyword: String) {
+        showProgress()
+        var englishKeyword = inko.ko2en(keyword)
 
-    private fun isValidKeyword(enteredKeyword: String): Boolean {
-        return isNotExceed() && isCorrectType(enteredKeyword) && isNotRegistered(enteredKeyword)
-    }
-
-    private fun isNotExceed(): Boolean {
-        if (KEYWORD_LIMIT > binding.tvRegisteredKeyword.text.toString().toInt()) return true
-        showMessage("키워드는 10개까지 등록 가능합니다.")
-        return false
-    }
-
-    private fun isCorrectType(enteredKeyword: String): Boolean {
-        var ps = Pattern.compile("^[0-9ㄱ-ㅎ가-힣]+$");
-        if (ps.matcher(enteredKeyword).matches()) return true
-        showMessage("한글과 숫자만 등록 가능합니다.")
-        return false
-    }
-
-    private fun isNotRegistered(enteredKeyword: String): Boolean {
-        if (!myKeywordList.contains(Keyword(enteredKeyword))) return true
-        showMessage("이미 등록된 키워드입니다.")
-        return false
-    }
-
-    private fun subscribe(enteredKeyword: String) {
-        var englishKeyword = inko.ko2en(enteredKeyword)
-
-        FirebaseMessaging.getInstance().subscribeToTopic(englishKeyword)
+        fcmReference.subscribeToTopic(englishKeyword)
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
                     var num = "1" // 기본값 1
-                    if (map.containsKey(enteredKeyword)) {
-                        num = (map.getValue(enteredKeyword).toInt() + 1).toString() // 구독자 수 +1
+                    if (map.containsKey(keyword)) {
+                        num = (map.getValue(keyword).toInt() + 1).toString() // 구독자 수 +1
                     }
 
-                    lifecycleScope.launch(Dispatchers.IO){
-                        databaseReference.child("keywords").child(enteredKeyword).setValue(num)
-                        //model.insert(Keyword(enteredKeyword))
-                    }
-
+                    databaseReference.child("keywords").child(keyword).setValue(num)
+                    viewModel.writeKeyword(keyword)
                 } else {
-                    showMessage("네트워크 상태가 불안정 합니다.")
+                    showToastMessage(resources.getString(R.string.network_error))
                 }
                 hideProgress()
             }
@@ -161,43 +133,32 @@ class KeywordActivity : BaseActivity<ActivityKeywordBinding>(R.layout.activity_k
         showProgress()
         var englishKeyword = inko.ko2en(keyword)
 
-        FirebaseMessaging.getInstance().unsubscribeFromTopic(englishKeyword)
+        fcmReference.unsubscribeFromTopic(englishKeyword)
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
-                    lifecycleScope.launch(Dispatchers.IO){
-                        var num = map.getValue(keyword).toInt() - 1 // 구독자 수 -1
-                        databaseReference.child("keywords").child(keyword).setValue(num.toString())
-                        //model.deleteKeywordByTitle(keyword)
-                    }
+                    var num = map.getValue(keyword).toInt() - 1 // 구독자 수 -1
+                    databaseReference.child("keywords").child(keyword).setValue(num.toString())
+                    viewModel.deleteKeyword(keyword)
                 } else {
-                    showMessage("네트워크 상태가 불안정 합니다.")
+                    showToastMessage(resources.getString(R.string.network_error))
                 }
                 hideProgress()
             }
     }
 
     override fun signUpListener(keyword: String) {
-        showProgress()
-        if (isValidKeyword(keyword)) {
-            subscribe(keyword)
-        } else {
-            hideProgress()
-        }
+        subscribe(keyword)
         binding.etKeyword.text = null
     }
 
     private fun showProgress() {
+        this.window.addFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
         binding.lottieViewSheep.visibility = View.VISIBLE
         binding.lottieViewSheep.playAnimation()
-        //this.window.addFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
     }
 
     private fun hideProgress() {
-        //this.window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
+        this.window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
         binding.lottieViewSheep.visibility = View.GONE
-    }
-
-    private fun showMessage(msg: String) {
-        Snackbar.make(binding.layoutKeyword, msg, Snackbar.LENGTH_SHORT).show()
     }
 }
