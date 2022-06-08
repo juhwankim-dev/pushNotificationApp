8d168de7ddb5aa7ec7f061230b680bdcb444011f
juhwankim-dev <juhwan.dev@gmail.com>
2022-06-09 01:19:41 +0900

    fix: :bug: 공지사항 Mapper 버그 수정(#21)

--- PushNotification/app/src/main/java/com/juhwan/anyang_yi/data/mapper/UnivMapper.kt
+++ PushNotification/app/src/main/java/com/juhwan/anyang_yi/data/mapper/UnivMapper.kt
@@ -1,6 +1,5 @@
 package com.juhwan.anyang_yi.data.mapper
 
-import android.util.Log
 import com.juhwan.anyang_yi.domain.model.Univ
 import com.juhwan.anyang_yi.present.config.Constants
 import com.juhwan.anyang_yi.present.utils.DateUtil
@@ -10,12 +9,13 @@ import org.jsoup.Jsoup
 object UnivMapper {
     operator fun invoke(responseBody: ResponseBody): List<Univ> {
         var list = arrayListOf<Univ>()
-
         var doc = Jsoup.parse(responseBody.string())
-        var noticeList = doc.select(".b-title-box ")
+        val headerList = doc.select(".b-top-box")
+        val noticeList = doc.select(".b-title-box ")
+        val headerSize = headerList.size
+        val noticeSize = noticeList.size
 
-        val size = noticeList.size
-        for(i in size - 10 until size) {
+        for(i in headerSize until noticeSize) {
             val href = noticeList[i].select("a[href]").attr("href")
             val title = noticeList[i].select("a[href]").attr("title")
             val date = "20" + noticeList[i].select(".b-date").text()
