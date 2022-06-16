afd33733eff467d6bab6139422cf25c5c94c342c
juhwankim-dev <juhwan.dev@gmail.com>
2022-06-16 13:59:46 +0900

    feat: :sparkles: 건의하기 기능 구현(#38)

--- PushNotification/app/src/main/java/com/juhwan/anyang_yi/present/views/setting/SettingAdapter.kt
+++ PushNotification/app/src/main/java/com/juhwan/anyang_yi/present/views/setting/SettingAdapter.kt
@@ -10,7 +10,8 @@ import com.juhwan.anyang_yi.present.views.setting.license.LicenseActivity
 import com.juhwan.anyang_yi.present.views.setting.profile.ProfileActivity
 
 class SettingAdapter : RecyclerView.Adapter<SettingAdapter.SettingViewHolder>() {
-    var items = listOf("앱 공지사항", "개발자 정보", "오픈소스 라이센스")
+    private lateinit var inquiryClickListener: InquiryClickListener
+    var items = listOf("앱 공지사항", "건의하기", "개발자 정보", "오픈소스 라이센스")
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SettingViewHolder {
         val binding = ItemSettingBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -22,33 +23,30 @@ class SettingAdapter : RecyclerView.Adapter<SettingAdapter.SettingViewHolder>() 
     }
 
     override fun onBindViewHolder(holder: SettingViewHolder, position: Int) {
-        holder.bind(items[position])
+        holder.bind(items[position], position)
     }
 
     inner class SettingViewHolder(private val binding: ItemSettingBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(str: String) {
-            var menuName = ""
-            var intent = Intent(itemView.context, AppNoticeActivity::class.java)
+        fun bind(name: String, position: Int) {
+            binding.name = name
 
-            when (str) {
-                "앱 공지사항" -> {
-                    menuName = "공지사항"
+            binding.clMenu.setOnClickListener {
+                when (position) {
+                    0 -> it.context.startActivity(Intent(itemView.context, AppNoticeActivity::class.java))
+                    1 -> inquiryClickListener.onClick()
+                    2 -> it.context.startActivity(Intent(itemView.context, ProfileActivity::class.java))
+                    3 -> it.context.startActivity(Intent(itemView.context, LicenseActivity::class.java))
                 }
-                "개발자 정보" -> {
-                    menuName = "개발자 정보"
-                    intent = Intent(itemView.context, ProfileActivity::class.java)
-                }
-                "오픈소스 라이센스" -> {
-                    menuName = "오픈소스 라이센스"
-                    intent = Intent(itemView.context, LicenseActivity::class.java)
-                }
-            }
-
-            binding.tvSetting.text = menuName
-            binding.layoutSetting.setOnClickListener {
-                it.context.startActivity(intent)
             }
         }
+    }
+
+    interface InquiryClickListener {
+        fun onClick()
+    }
+
+    fun setInquiryClickListener(inquiryClickListener: InquiryClickListener) {
+        this.inquiryClickListener = inquiryClickListener
     }
 }
--- PushNotification/app/src/main/java/com/juhwan/anyang_yi/present/views/setting/SettingFragment.kt
+++ PushNotification/app/src/main/java/com/juhwan/anyang_yi/present/views/setting/SettingFragment.kt
@@ -6,15 +6,35 @@ import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.juhwan.anyang_yi.R
 import com.juhwan.anyang_yi.databinding.FragmentSettingBinding
+import com.juhwan.anyang_yi.present.config.ApplicationClass.Companion.databaseReference
 import com.juhwan.anyang_yi.present.config.BaseFragment
+import com.juhwan.anyang_yi.present.utils.DateUtil
+import com.juhwan.anyang_yi.present.views.setting.inquiry.InquiryDialog
+import com.juhwan.anyang_yi.present.views.setting.inquiry.SendClickListener
 
-class SettingFragment : BaseFragment<FragmentSettingBinding>(R.layout.fragment_setting) {
+class SettingFragment : BaseFragment<FragmentSettingBinding>(R.layout.fragment_setting), SendClickListener {
+    private lateinit var settingAdapter: SettingAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding!!.rvSetting.adapter = SettingAdapter()
+        initView()
+        initEvent()
+    }
+
+    private fun initView() {
+        settingAdapter = SettingAdapter()
+        binding!!.rvSetting.adapter = settingAdapter
         binding!!.rvSetting.layoutManager = LinearLayoutManager(context)
         binding!!.tvVersion.text = "현재 버전 " + getAppVersion(requireContext())
+    }
+
+    private fun initEvent() {
+        settingAdapter.setInquiryClickListener(object : SettingAdapter.InquiryClickListener{
+            override fun onClick() {
+                InquiryDialog(requireActivity(), this@SettingFragment).createDialog()
+            }
+        })
     }
 
     private fun getAppVersion(context: Context): String? {
@@ -26,6 +46,11 @@ class SettingFragment : BaseFragment<FragmentSettingBinding>(R.layout.fragment_s
 
         }
         return versionName
+    }
+
+    override fun sendClickListener(inquiry: String) {
+        databaseReference.child("inquiry").child(DateUtil.getLocalDateTime()).setValue(inquiry)
+        showToastMessage("소중한 의견 감사합니다!")
     }
 }
 
