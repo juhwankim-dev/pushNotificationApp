bce3c885c1e33902ac1837fc383025b6724ad3ca
juhwankim-dev <juhwan.dev@gmail.com>
2022-06-16 14:00:29 +0900

    refactor: :zap: DateUtil 파일 코드 정리(#38)

--- PushNotification/app/src/main/java/com/juhwan/anyang_yi/present/utils/DateUtil.kt
+++ PushNotification/app/src/main/java/com/juhwan/anyang_yi/present/utils/DateUtil.kt
@@ -2,31 +2,25 @@ package com.juhwan.anyang_yi.present.utils
 
 import java.text.SimpleDateFormat
 import java.time.LocalDate
+import java.time.LocalDateTime
 
 object DateUtil {
     private val sdf = SimpleDateFormat("yyyy-MM-dd")
-    private var today = sdf.parse("${LocalDate.now()} 00:00:00")
+    private var now = sdf.parse("${LocalDate.now()} 00:00:00")
 
-    fun millisecondToDate(millisecond: Long): String {
-        return sdf.format(millisecond)
-    }
-
-    fun getLeftDay(millisecond: Long): Int {
-        return getLeftDay(millisecondToDate(millisecond))
+    fun millisecondToDate(millisecond: Long): String = sdf.format(millisecond)
+    fun getLeftDay(millisecond: Long): Int = getLeftDay(millisecondToDate(millisecond))
+    fun getFirstDayOfThisYear(): String = "${LocalDate.now().year}-01-01"
+    fun getLastDayOfThisYear(): String = "${LocalDate.now().year}-12-31"
+    fun getLocalDateTime(): String {
+        var dateTime = "${LocalDateTime.now()}"
+        return dateTime.replace(".", "")
     }
 
     fun getLeftDay(dateString: String): Int {
         var writeDate = sdf.parse("$dateString 00:00:00")
-        var calculateDate = (today.time - writeDate.time) / (60 * 60 * 24 * 1000)
+        var calculateDate = (now.time - writeDate.time) / (60 * 60 * 24 * 1000)
 
         return calculateDate.toInt()
-    }
-
-    fun getFirstDayOfThisYear(): String {
-        return "${LocalDate.now().year}-01-01"
-    }
-
-    fun getLastDayOfThisYear(): String {
-        return "${LocalDate.now().year}-12-31"
     }
 }
